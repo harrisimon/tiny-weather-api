85927fa32e8ec89ba172a2044c7301f5d0676487
harrisimon <harrisimon@gmail.com>
2023-01-04 21:09:40 -0500

    added route to get all reviews from one user

--- app/routes/weather_routes.js
+++ app/routes/weather_routes.js
@@ -2,17 +2,20 @@ const express = require("express")
 const passport = require("passport")
 
 const Weather = require("../models/weather")
-const User = require('../models/user')
+const User = require("../models/user")
+const Review = require("../models/review")
 
 const customErrors = require("../../lib/custom_errors")
 const handle404 = customErrors.handle404
 const requireOwnership = customErrors.requireOwnership
 const removeBlanks = require("../../lib/remove_blank_fields")
+const { ObjectId } = require("mongodb")
 const requireToken = passport.authenticate("bearer", { session: false })
 const router = express.Router()
 
 router.post("/weather", (req, res, next) => {
 	Weather.create(req.body.weather)
+		.then(handle404)
 		.then((weather) => {
 			res.status(201).json({
 				weather: weather.toObject(),
@@ -26,6 +29,7 @@ router.get("/weather", (req, res, next) => {
 		.then((weather) => {
 			return weather.map((weather) => weather.toObject())
 		})
+
 		.then((weather) => {
 			res.status(200).json({ weather: weather })
 		})
@@ -44,27 +48,21 @@ router.get("/weather/:id", (req, res, next) => {
 
 router.get("/latest", (req, res, next) => {
 	Weather.find({})
-        
+
 		.sort({ _id: -1 })
 		.limit(1)
-        .populate({
-            path: 'reviews',
-            populate: {
-                path:'author',
-                model:'User',
-                    populate: {
-
-                        path:'email'
-                    }
-            }
-        })
+		.populate({
+			path: "reviews",
+			populate: {
+				path: "author",
+				model: "User",
+				populate: {
+					path: "email",
+				},
+			},
+		})
 		.then((weather) => {
-			// weather.logTime = new Date(weather[0].createdAt).toLocaleString('en-us')
-			// console.log("logtime",weather[0].reviews)
 			res.status(200).json({ weather: weather })
-			// console.log("created at",weather[0].createdAt)
-			// const date = new Date(`${weather[0].createdAt}`).toLocaleString('en-us')
-			// console.log(date)
 		})
 		.catch(next)
 })
@@ -77,11 +75,42 @@ router.get("/today", (req, res, next) => {
 	Weather.find({
 		createdAt: {
 			$gte: yesterday,
-		}, 
+		},
 	})
-    .then((weather) => {
-        res.status(200).json({weather:weather})
-    })
+		.then(handle404)
+		.then((weather) => {
+			res.status(200).json({ weather: weather })
+		})
+		.catch(next)
+})
+
+router.get("/my-posts", requireToken, (req, res, next) => {
+	const author = req.user.id
+	console.log(author)
+	Weather.aggregate([
+		{
+			$match: {
+				reviews: { $elemMatch: { author: ObjectId(`${author}`) } },
+			},
+		},
+		{ $unwind: "$reviews" },
+		{ $match: { "reviews.author": { $eq: ObjectId(`${author}`) } } },
+	])
+
+		.then(handle404)
+		.then((reviews) => {
+			res.status(200).json({ reviews: reviews })
+		})
+})
+
+router.delete("/delete/:id", (req, res, next) => {
+	Weather.findById(req.params.id)
+		.then(handle404)
+		.then((weather) => {
+			weather.deleteOne()
+		})
+		.then(() => res.sendStatus(204))
+		.catch(next)
 })
 
 module.exports = router
