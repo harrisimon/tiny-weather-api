de8c5875bdab827c81e47247a0ed52bbb93ac0dd
harrisimon <harrisimon@gmail.com>
2023-01-09 11:25:36 -0500

    added delete review route

--- app/routes/review_routes.js
+++ app/routes/review_routes.js
@@ -8,6 +8,7 @@ const customErrors = require('../../lib/custom_errors')
 const handle404 = customErrors.handle404
 const requireOwnership = customErrors.requireOwnership
 const removeBlanks = require('../../lib/remove_blank_fields')
+const weather = require('../models/weather')
 const requireToken = passport.authenticate('bearer', { session: false })
 const router = express.Router()
 
@@ -18,16 +19,30 @@ router.post('/review/:weatherId', requireToken,removeBlanks, (req, res, next) =>
     const review = req.body
 
     const weatherId = req.params.weatherId
-    console.log("the review being saved",review)
+    // console.log("the review being saved",review)
     
     Weather.findById(weatherId)
         .then(handle404)
         .then(weather => {
-            console.log("review before push", typeof review.author)
+            // console.log("review before push", typeof review.author)
             weather.reviews.push(review)
             return weather.save()
         })
         .then(weather => res.status(201).json({weather: weather}))
+        .catch(next)
+})
+
+router.delete('/review/:weatherId/:reviewId', requireToken, (req, res, next) => {
+    const {weatherId, reviewId} = req.params
+    Weather.findById(weatherId)
+        .then(handle404)
+        .then(weather => {
+            const theReview = weather.reviews.id(reviewId)
+            // requireOwnership(req, weather)
+            theReview.remove()
+            return weather.save()
+        })
+        .then(weather => res.sendStatus(204))
         .catch(next)
 })
 
